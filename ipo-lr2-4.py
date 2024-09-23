1dc1dcfa96b2e124531459a0c9c3de89b56151d0
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-09-23 19:44:25 +0300

    Update ipo-lr2-4.py

--- ipo-lr2-4.py
+++ ipo-lr2-4.py
@@ -1,7 +1,7 @@
-from math import sqrt
 from math import asin
+from math import sqrt
 x = int(input("x = "))
 y = int(input("y = "))
-z = int(input("z = "))
-b = sqrt(10 * (x ** (1/3) + x ** (y + 2)))*((asin(z) ** 2) - abs(x - y))
+z = int(input("z (от -1 до 1) = "))
+b = sqrt(10 * (x ** (1/3) + (x ** (y + 2)))) * ((asin(z) ** 2) - abs(x - y))
 print(b)
